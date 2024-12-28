0da4e3c07a23bc9f11abba2b89e9374a0d3e50f4
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-12-28 17:14:23 -0500

    invalid commands attempt 1

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatalf("error reading stdin")
+	}
+	fmt.Printf("%s: command not found\n", input[:len(input)-1])
 }
