91b66f7217f107cf150a8419254463bc6568740c
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-12-28 17:15:32 -0500

    repl attempt 1

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		log.Fatalf("error reading stdin")
+	for {
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatalf("error reading stdin")
+		}
+		fmt.Printf("%s: command not found\n", input[:len(input)-1])
 	}
-	fmt.Printf("%s: command not found\n", input[:len(input)-1])
 }
